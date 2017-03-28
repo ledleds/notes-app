7450c71e8fa26478fd3775ab1eb16d29418fd2b1
Vicky Ledsom <victorialedsom@gmail.com>
2017-03-28 18:06:54 +0100

    (WIP) 6- Add initial controller code

--- note-controller.js
+++ note-controller.js
@@ -1,6 +1,26 @@
-function getDiv() {
-  var element = document.getElementById("app")
-  element.innerHTML = "Howdy" // innerHTML completely replaces the contents of the element we called it on. 
-}
+(function(exports) {
 
-getDiv();
+  function NoteController(noteList) {
+    this.noteList = noteList
+  }
+
+  NoteController.prototype.createNoteListView = function() {
+    this.noteListView = new NoteListView(this.noteList)
+  };
+
+  NoteController.prototype.addNoteToList = function () {
+    this.noteList.addNote("Favourite drink: gin")
+  };
+
+  NoteController.prototype.getHTML = function() {
+    var element = document.getElementById("app")
+    element.innerHTML = this.noteListView.generateHTML()
+  };
+  exports.NoteController = NoteController
+})(this);
+
+var noteList = new NoteList();
+var noteController = new NoteController(noteList);
+noteController.addNoteToList();
+noteController.createNoteListView();
+noteController.getHTML();
