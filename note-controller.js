155cf0f025c8b199d1979a8ecb5d3130f965b533
Vicky Ledsom <victorialedsom@gmail.com>
2017-03-31 18:26:32 +0100

    (WIP) Moved has change into controller- Shownote needs fixing

--- note-controller.js
+++ note-controller.js
@@ -2,12 +2,13 @@
 (function(exports) {
   function NoteController(noteList) { //Should I be passing in NoteListView instead to adhere to SRP?
     this.noteList = noteList;
+    // controller = this;
   }
 
   NoteController.prototype.createNoteListView = function(noteListView = new NoteListView(this.noteList)) {
     this.noteListView = noteListView
   };
-  
+
   NoteController.prototype.addNoteToList = function(text) {
     this.noteList.addNote(text);
   };
@@ -15,6 +16,28 @@
   NoteController.prototype.insertHTML = function(element = document.getElementById("app")) {
     var element = element;
     element.innerHTML = this.noteListView.generateHTML();
+  };
+
+  NoteController.prototype.makeUrlChangeShowNoteForCurrentPage = function() {
+    window.addEventListener("hashchange", this.showNoteForCurrentPage.bind(this));
+  };
+
+  NoteController.prototype.showNoteForCurrentPage = function(){
+    console.log(this)
+    // console.log(controller)
+
+    this.showNote(this.getNoteFromUrl(window.location));
+  };
+
+  NoteController.prototype.getNoteFromUrl = function(location) {
+    return location.hash.split("#")[1];
+  };
+
+  NoteController.prototype.showNote = function(id2) {
+    console.log(this.noteList._notes.id)
+    document
+    .getElementById("note-content")
+    .innerHTML = this.noteList._notes[id2].text;
   };
 
   exports.NoteController = NoteController;
